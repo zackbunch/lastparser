00bea68e8f2841b97439f2429841d40c349b3082
Zack Bunch <zackbunch@MacBook-Pro.local>
2020-04-29 00:19:50 -0400

    regex test for valif wtmp file

---
+++ last.py
@@ -0,0 +1,16 @@
+#read the last line of the file and check if it is a valid
+# wtmp file if not write to error file
+
+#^\b(\w*wtmp begins\w*)\b\s+(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s{1,2}\d{1,2}\s\d{2}:\d{2}:\d{2}\s+\d{4}$
+import re
+import os
+
+with open('(Unix format) 2016 10 29 valid data.txt', 'rb') as f:
+    f.seek(-2, os.SEEK_END)
+    while f.read(1) != b'\n':
+         f.seek(-2, os.SEEK_CUR)
+    last_line = f.readline().decode()
+    if re.match(r'^\b(\w*wtmp begins\w*)\b\s+(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s{1,2}\d{1,2}\s\d{2}:\d{2}:\d{2}\s+\d{4}$',last_line):
+        print('valid')
+    else:
+        print('invalid')
